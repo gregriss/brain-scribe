c322541ba3750f9e4ecb9a019bce4f7942663918
Greg Riss <gregriss23@gmail.com>
2021-05-12 18:04:43 -0400

    install dotenv package, add google credentials to the project (.gitignore), install google speech library; successful API call in speech.js

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const passport = require("./config/passport");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// dotenv package
+require('dotenv').config()
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "the secret that always changes", resave: true, saveUninitialized: true })
