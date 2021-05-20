fa86b3a48c4d0f2558948a0d99ff2cd3f1e50aeb
Greg Riss <gregriss23@gmail.com>
2021-05-20 14:48:19 -0400

    add react-speech-recognition package; SpeechRec component (unused)

--- client/src/pages/Ideas.js
+++ client/src/pages/Ideas.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+// import PropTypes from "prop-types";
+// import SpeechRecognition from "react-speech-recognition";
 import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import DragandDrop from "../components/DragandDrop";
