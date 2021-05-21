82f4441b8cecef44acb38deb6ca521c5034f3839
Greg Riss <gregriss23@gmail.com>
2021-05-21 12:28:57 -0400

    deploying with google credentials buildpack

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 250, clear: "both", paddingTop: 100, marginTop: '1rem', textAlign: "center", color: "hsl(239, 75%, 40%)" }}
+      style={{ height: 250, clear: "both", paddingTop: 100, marginTop: '1rem', textAlign: "center", color: "hsl(239, 75%, 40%)", boxShadow: 'inset 0px 0px 15px #a1a1a1' }}
       className="jumbotron"
     >
       {children}
